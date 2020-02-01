fac12c686f70009528f3f70096e1fe996b58d9b9
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-01 17:25:28 +0000

    Updates

---
+++ precache-manifest.b4b0234119ebf32638ddb957eede47a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1845e7b16d0adda10eb3cdb6547de214",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "80210c15a22233d40fa3",
+    "url": "/shopping_cart/static/css/main.d5eeab37.chunk.css"
+  },
+  {
+    "revision": "a31459cbd2bd084c2aed",
+    "url": "/shopping_cart/static/js/2.823deffa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shopping_cart/static/js/2.823deffa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80210c15a22233d40fa3",
+    "url": "/shopping_cart/static/js/main.3408f64c.chunk.js"
+  },
+  {
+    "revision": "0b7d9791ffcd7e994fd9",
+    "url": "/shopping_cart/static/js/runtime-main.c7880ae2.js"
+  }
+]);
